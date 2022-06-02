18729806664921319d7545fecda1eccc0eb062d9
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-06-03 03:24:31 +0530

    setup slate (old version)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
-  );
+import { useState } from "react";
+import { Editor } from "slate-react";
+import { Value } from 'slate';
+
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: "block",
+        type: "paragraph",
+        nodes: [
+          {
+            object: "text",
+            leaves: [
+              {
+                text: "A line of text in a paragraph."
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+} as any);
+
+const App = () => {
+  const [value, setValue] = useState(initialValue);
+
+  return <Editor value={value} onChange={(opts) => setValue(opts.value)}/>
 }
 
 export default App;
