2e5d4aa9ed55517edeafaec07ecea77190b33848
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-06-04 01:44:19 +0530

    Data syncing enabled along with a id

--- web/src/SyncingEditor.tsx
+++ web/src/SyncingEditor.tsx
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import { Editor } from "slate-react";
 import { initialValue } from "./slateInitialValue";
+import Mitt from "mitt";
 
 interface Props {
 
 }
 
+const emitter = Mitt();
+
 export const SyncingEditor: React.FC<Props> = () => {
     const [value, setValue] = useState(initialValue);
+    const editor = useRef<Editor | null>(null);
+    const id = useRef(`${Date.now()}`);
+    //const remote = useRef(null);
 
-    return <Editor value={value} onChange={(opts) => setValue(opts.value)} />
+    useEffect(() => {
+        emitter.on("*", (type) => {
+            if(id.current !== type) {
+                console.log("Change has happened in other editor")
+            }
+        })
+    }, []);
+
+    return (
+        <Editor
+            ref={editor} 
+            value={value}  
+            onChange={(opts) => {
+                setValue(opts.value)
+
+                const ops = opts.operations
+                    .filter(o => {
+                        if(o) {
+                            return (
+                                o.type !== "set_selection" &&
+                                o.type !== "set_value" &&
+                                (!o.data || !o.data.has("source"))
+                            );
+                        }
+
+                        return false;
+                    })
+                    .toJS()
+                    .map((o: any) => ({...o, data: { source: "one" }}));
+
+                if(ops.length) {
+                    emitter.emit(id.current, ops);
+                }
+            }} 
+        />
+    )
 }
