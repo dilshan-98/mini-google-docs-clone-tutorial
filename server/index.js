01488c61c0e1f23605a7601a763a3e6f9436a8df
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-06-17 23:53:52 +0530

    Backend with sockets which emits the operations to sync with editors

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 io.on('connection', function(socket) {
-    console.log(('user connected'))
-})
+    socket.on("new-operations", function (data) {
+        io.emit("new-remote-operations",data);
+    });
+});
 
 http.listen(4000, function () {
     console.log('listening on port 4000')
-})
+});
